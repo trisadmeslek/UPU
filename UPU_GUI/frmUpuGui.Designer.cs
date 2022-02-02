6ce31ee4dbf6cce624b42f0de0f886b41abc6e91
Eray Aydın <Eray.Aydin@kocholding.onmicrosoft.com>
2022-02-02 16:50:52 +0300

    Upu Gui Added

---
+++ UPU_GUI/frmUpuGui.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace UPU_GUI
+{
+    partial class frmUpuGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnUnpack = new System.Windows.Forms.Button();
+            this.txtPackage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(419, 12);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(29, 23);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "...";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnUnpack
+            // 
+            this.btnUnpack.Location = new System.Drawing.Point(338, 41);
+            this.btnUnpack.Name = "btnUnpack";
+            this.btnUnpack.Size = new System.Drawing.Size(75, 23);
+            this.btnUnpack.TabIndex = 1;
+            this.btnUnpack.Text = "Unpack";
+            this.btnUnpack.UseVisualStyleBackColor = true;
+            this.btnUnpack.Click += new System.EventHandler(this.btnUnpack_Click);
+            // 
+            // txtPackage
+            // 
+            this.txtPackage.Location = new System.Drawing.Point(12, 14);
+            this.txtPackage.Name = "txtPackage";
+            this.txtPackage.ReadOnly = true;
+            this.txtPackage.Size = new System.Drawing.Size(401, 20);
+            this.txtPackage.TabIndex = 2;
+            // 
+            // frmUpuGui
+            // 
+            this.AcceptButton = this.btnUnpack;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 74);
+            this.Controls.Add(this.txtPackage);
+            this.Controls.Add(this.btnUnpack);
+            this.Controls.Add(this.btnChoose);
+            this.Name = "frmUpuGui";
+            this.Text = "UPU GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnUnpack;
+        private System.Windows.Forms.TextBox txtPackage;
+    }
+}
+
---
+++ UPU_GUI/frmUpuGui.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPU_GUI
+{
+    public partial class frmUpuGui : Form
+    {
+        public frmUpuGui()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog1.FileName))
+            {
+                FileInfo info = new FileInfo(openFileDialog1.FileName);
+
+                if (info.Extension != ".unitypackage")
+                {
+                    ShowMessageForRightExtension();
+                    return;
+                }
+                txtPackage.Text = openFileDialog1.FileName;
+                btnUnpack.Focus();
+            }
+        }
+
+        void ShowMessageForRightExtension()
+        {
+            ShowMessage("Please choose a file with \".unitypackage\" extension");
+        }
+
+        void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void btnUnpack_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtPackage.Text))
+                return;
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "UpuConsole.exe",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                Arguments = "-i " + "\"" + txtPackage.Text + "\""
+            });
+        }
+    }
+}
